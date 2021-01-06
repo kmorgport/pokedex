5aec85c9d970819cc06638463d1244f0aba0b99d
Kole Morgan <kmorgan0057@gmail.com>
2021-01-05 20:47:55 -0600

    import sprites into pokedex from json

--- js/pokedex.js
+++ js/pokedex.js
@@ -12,9 +12,32 @@ $.get(dex).done(function(data, status, jqXhr){
     console.log("This function runs!");
 });
 
-const pokemon = "https://pokeapi.co/api/v2/pokemon/bulbasaur/"
+const pokemon = "https://pokeapi.co/api/v2/pokemon/"
 
-$.get(pokemon).done(function(data, status, jqXhr){
+$('#userSearch').click(function(e){
+  e.preventDefault();
+  let query = $("#searchValue").val().toLowerCase();
+  $.get(pokemon+query+"/").done(function(data,status){
+      if($('#picture').find('img').length){
+          $('#picture img').attr("src",data.sprites['front_default'])
+      }else{$('#picture').append("<img src='"+data.sprites['front_default']+"'>")}
+  })
+})
+
+
+// $.get(pokemon).done(function(data, status, jqXhr){
+//     // alert("Went well.");
+//     console.log(data)
+// }).fail(function(jqXhr, status, error){
+//     // alert("Error error");
+//     console.log("Response status: "+ status);
+//     console.log("Error object: "+ error);
+// }).always(function(){
+//     console.log("This function runs!");
+// });
+
+const entries = "data/pokedex.json";
+$.get(entries).done(function(data, status, jqXhr){
     // alert("Went well.");
     console.log(data)
 }).fail(function(jqXhr, status, error){
